eeef5659c17af08ee7ab898f4ebc233cb3b8a1c4
elane1980 <eliselanes1@gmail.com>
2025-03-27 20:57:13 +0000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import BookSalesTracker from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BookSalesTracker from './App';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BookSalesTracker />
   </React.StrictMode>
